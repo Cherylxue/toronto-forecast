a1d771223eb3c4d2959c94f78dfc6c3ecf2d590c
Chun Xue <49353300+Cherylxue@users.noreply.github.com>
2021-06-08 17:45:50 -0400

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -15,29 +15,34 @@ function Weather(props) {
   return (
     <>
       <h3>{days[day]}</h3>
-      {data.map((ele) => {
-        let date = new Date(ele.dt_txt);
-        let icon;
+      {data.length === 0 ? (
+        <h4>No data!</h4>
+      ) : (
+        data.map((ele) => {
+          let date = new Date(ele.dt_txt);
+          let icon;
 
-        if (ele.weather[0].main === "Clouds") {
-          icon = "wi-cloud.svg";
-        } else if (ele.weather[0].main === "Rain") {
-          icon = "wi-rain.svg";
-        } else if (ele.weather[0].main === "Clear") {
-          icon = "wi-day-sunny.svg";
-        }
-        return (
-          <div key={data.indexOf(ele)}>
-            Time:{date.getHours()}:{date.getMinutes()}
-            <br />
-            Weather:{ele.weather[0].main}
-            <br />
-            Temperature:{(ele.main.temp - 273.15).toFixed(2)} Celsius
-            <br />
-            <img src={icon} alt="weather" height="100px" width="100px"></img>
-          </div>
-        );
-      })}
+          if (ele.weather[0].main === "Clouds") {
+            icon = "wi-cloud.svg";
+          } else if (ele.weather[0].main === "Rain") {
+            icon = "wi-rain.svg";
+          } else if (ele.weather[0].main === "Clear") {
+            icon = "wi-day-sunny.svg";
+          }
+
+          return (
+            <div key={data.indexOf(ele)}>
+              Time:{date.getHours()}:{date.getMinutes()}
+              <br />
+              Weather:{ele.weather[0].main}
+              <br />
+              Temperature:{(ele.main.temp - 273.15).toFixed(2)} Celsius
+              <br />
+              <img src={icon} alt="weather" height="100px" width="100px"></img>
+            </div>
+          );
+        })
+      )}
     </>
   );
 }
